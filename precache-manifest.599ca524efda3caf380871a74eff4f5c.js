8f692612586026ee397726dc92aa95554edd24e5
Mizraim <mizraim.martinez@gmail.com>
2019-04-02 13:43:06 -0600

    Updates

---
+++ precache-manifest.599ca524efda3caf380871a74eff4f5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fef54414303e0dc56dd2",
+    "url": "/noticias-zim/static/css/main.3bfeaec9.chunk.css"
+  },
+  {
+    "revision": "fef54414303e0dc56dd2",
+    "url": "/noticias-zim/static/js/main.df2774a6.chunk.js"
+  },
+  {
+    "revision": "53f5411b268f545bc6ae",
+    "url": "/noticias-zim/static/js/runtime~main.acbd03ea.js"
+  },
+  {
+    "revision": "ee0e1958fa4478992b29",
+    "url": "/noticias-zim/static/js/2.c33cc127.chunk.js"
+  },
+  {
+    "revision": "aaebabeb99f56e0c7b46d0f3c25c0b45",
+    "url": "/noticias-zim/index.html"
+  }
+];
