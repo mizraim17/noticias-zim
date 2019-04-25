f4052f24aa0a914ae3df9b1e5932862b23bdf725
kenshin <mizraim17>
2019-04-25 01:17:51 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/noticias-zim/precache-manifest.853595a2899a0b452905218ef6e041a4.js"
+  "/noticias-zim/precache-manifest.01348afa46101c7be3e1478b54729644.js"
 );
 
 workbox.clientsClaim();
