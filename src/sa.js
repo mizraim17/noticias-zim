794327f26d3d68e3e5c565462fd0c18514ea9815
mizraim17 <mizraim.martinez@gmail.com>
2019-06-06 14:38:44 -0500

    actualziacion de dependencias

--- src/sa.js
+++ src/sa.js
@@ -1,9 +1,1 @@
-import {Card} from "react-materialize";
 
-<Card className='blue-grey darken-1' textClassName='white-text' title={props.note.title}>
-
-  <img src={props.note.urlToImage} className="circle align-center" width="50" alt=""/>
-  actions={[<a href='#'>This is a link</a>]}>
-  <h4>  {props.note.title}</h4>
-  {props.note.content}
-</Card>
